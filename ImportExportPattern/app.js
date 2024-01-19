a2bfa3153e95292b7abc47c6fd9603194cc3208b
safinhr <hrsafin2434@gmail.com>
2024-01-19 19:37:59 +0600

    node.js : Import Export Pattern

---
+++ ImportExportPattern/app.js
@@ -0,0 +1,44 @@
+// pattern 1
+
+// module.exports = (a,b) => {
+//     return a + b
+// }
+
+// const add = (a,b) => {
+//     return a + b
+// }
+
+
+// const sub = (a,b) => {
+//     return a - b
+// }
+
+// pattern 2
+
+// module.exports = {
+//     add,
+//     sub
+// }
+
+
+// pattern 3
+
+// module.exports.add = (a,b) => {
+//     return a + b
+// }
+
+
+// module.exports.sub = (a,b) => {
+//     return a - b
+// }
+
+// pattern 4
+exports.add = (a,b) => {
+    return a + b
+}
+
+
+exports.sub = (a,b) => {
+    return a - b
+}
+
---
+++ ImportExportPattern/index.js
@@ -0,0 +1,9 @@
+// const add = require("./app")
+
+// console.log(console.log(add(4,5)))
+
+
+const sum = require("./app")
+
+console.log(sum.add(10,10))
+console.log(sum.sub(10,10))
